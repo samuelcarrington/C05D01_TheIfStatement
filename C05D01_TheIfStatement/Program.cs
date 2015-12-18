f7e4122fd23db9c46cb8811bf979a2f593d3f0f5
Samuel Carrington <samuelcarrington@gmail.com>
2015-12-18 17:02:57 -0500

    Meaningful commit message

---
+++ C05D01_TheIfStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C05D01_TheIfStatement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter two numbers (on separate lines).");
+
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int biggerNumber = firstNumber;
+            if (secondNumber > firstNumber) ;
+            {
+                biggerNumber = secondNumber;
+            }
+
+            Console.WriteLine("The bigger number is: {0}", biggerNumber);
+            Console.ReadLine();
+        }
+    }
+}
